d6f0ebcbc8d1cfbdabe56177daaabb03c2a4534c
SarahGieben <90245294+SarahGieben@users.noreply.github.com>
2022-11-09 12:55:21 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -26,16 +26,20 @@ Blauw
   <summary>De eerste versie/schets van je ontwerp & je persoonlijke uitdaging</summary>
 
   ### De eerste versie/schets:
-  <img src="readme-images/dummy-plaatje.jpg" width="375px" alt="eerste versie/schets">
   <img src="readme-images/pacman-schets.jpg" width="375px" alt="Pacman schets">
   <img src="readme-images/spiderman-schets.jpg" width="375px" alt="Spiderman schets">
+  <img src="readme-images/Aardappelhoofd-schets.jpg" width="375px" alt="Pacman schets">
+
+  ### Mijn idee en kenmerken van mijn karakter:
+  Mijn idee voor deze opdracht is uiteindelijk meneer en mevrouw aardappelhoofd geworden. Je kan dan interacteren met het karakter door zijn ledenmate, ogen, accessoires er af te halen, er weer op te zetten. Zo kan je dingen mixen, niet gebruiken
+
 
   ### Je ambitie: 
   Aan deze technieken/punten wil ik werken:
-  - punt 1
-  - punt 2
-  - nog een punt
-  - ...
+  - Ik wil gewoon uberhaupt een beetje beter worden in vooral html css. Als dat me lukt misschien iets meer ook js. Omdat ik daar allemaal nog niet zo goed in ben, vooral in js.
+  - Ik wil de lat voor mezelf eerst niet te hoog leggen. Want dat heb ik vaak met code opdrachten gedaan waardoor ik het niet optijd af kreeg. Als eerste is mijn doel om een werkende interactie site te bouwen en daarna pas leuke extra's toevoegen als ik daar nog tijd voor heb.
+  - Ook code die ik regelmatig moet gebruiken zoals plaatsing van elementen, het responsive maken etc. dat wil ik een beetje beter uit mijn hoofd kunnen doen.
+  - Met deze opdracht hoop ik als laatste gewoon een beetje meer plezier te kunnen hebben in de opdracht. Vaak met code opdrachten, omdat ik er niet zo goed in ben, heb ik als designer een geweldig idee in mijn hoofd hoe ik de opdracht wil hebben maar dan raak ik gestrest omdat ik het niet zo in code kan krijgen als dat ik het in mijn hoofd heb. Dus ik hoop er iets meer plezier uit te kunnen halen.
  
 </details>
 
@@ -50,19 +54,22 @@ Blauw
   </summary>
 
   ### Bevinding 1:
-  Omschrijving van wat er nog niet orde was (tekst en afbeeding(en)).
+  <img src="readme-images/pacman-schets.jpg" width="375px" alt="Pacman schets">
+  Als eerste mijn Pacman schets. De schets was nog teveel een 'schets', net zoals het idee. Het leek me een moeilijk idee om te realiseren. Wel vond ik dat de iconische pacman geluidjes door de jaren heen erg leuk hadden geweest.
 
   #### oplossing:
-  Omdat mijn ontwerp nog teveel op een servetje leek en mijn andere idee iets zelf moest verzinnen heb ik een nieuw idee geschetst
+  Een nieuw idee verzinnen.
 
 
 
   ### Bevinding 2:
-  Omschrijving van wat er nog niet orde was (tekst en afbeeding(en)).
+  <img src="readme-images/spiderman-schets.jpg" width="375px" alt="Spiderman schets">
+  Ook mijn spiderman schets was teveel een 'schets'. Het idee vond ik een goed idee omdat het echt iets karakteristieks is van spiderman. Alleen ik had het idee niet zelf bedacht.
 
   #### oplossing:
-  Beschrijving hoe je het hebt hebt opgelost of als het niet gelukt is hoe je het zou oplossen (tekst en afbeeding(en)).
+  Mijn oplossing was dus een nieuw idee verzinnen en daarvoor ook betere schetsen maken. Dit is uiteindelijk mijn nieuwe idee geworden die ik ga maken:
 
+  <img src="readme-images/Aardappelhoofd-schets.jpg" width="375px" alt="Pacman schets">
 
 
   ### Bevinding 3:
--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,130 @@
 // JavaScript Document
 console.log("Howdy!");
+
+var welkePotato = "aardappel";
+
+/**************************/
+/*   Verplaatsen Meneer   */
+/*   Met dank aan Sanne   */
+/**************************/
+
+const deHoedM = document.querySelector("button:first-of-type");
+const deOgenM = document.querySelector("button:nth-of-type(2)");
+const deSnorM = document.querySelector("button:nth-of-type(3)");
+const deArmenM = document.querySelector("button:nth-of-type(4)");
+const deSchoenenM = document.querySelector("button:nth-of-type(5)");
+
+deHoedM.addEventListener("click", zetHoedOp);
+deOgenM.addEventListener("click", zetOgenOp);
+deSnorM.addEventListener("click", zetSnorOp);
+deArmenM.addEventListener("click", zetArmenOp);
+deSchoenenM.addEventListener("click", zetSchoenenOp);
+
+function zetHoedOp() {
+	deHoedM.classList.toggle("op");
+}
+function zetOgenOp() {
+	deOgenM.classList.toggle("op");
+}
+function zetSnorOp() {
+	deSnorM.classList.toggle("op");
+}
+function zetArmenOp() {
+	deArmenM.classList.toggle("op");
+}
+function zetSchoenenOp() {
+    deSchoenenM.classList.toggle("op");
+}
+
+
+
+
+/***************************/
+/*     Buttons ophalen     */
+/*    Met dank aan Sasja   */
+/***************************/
+
+allButtons = document.querySelectorAll("section > button");
+// met querySelectorAll haal je alle buttons op
+
+allButtons.forEach((button) => {
+    //voor elke button, voer een stukje code uit
+    button.addEventListener("click", ()=> {
+
+        var propDieNietOpIs = document.querySelector("section > button:not(.op)");
+
+
+        if (propDieNietOpIs == null ) {
+            // props niet meer klikbaar
+            allButtons.forEach( button => {
+                button.setAttribute("inert", "true");
+            });
+
+            // aardappel klikbaar maken
+            var aardappelKnop = document.querySelector("article > button");
+            aardappelKnop.addEventListener("click", transform, {once:true});
+
+            // toon info
+            const gaPraten = document.querySelector("article:nth-of-type(2) section");
+            gaPraten.classList.add('praten');
+        }
+    })
+})
+
+
+
+/**************************/
+/*     Plastic maken      */
+/*                        */
+/**************************/
+
+function transform() {
+    if (welkePotato == "aardappel") {
+        welkePotato = "plastic";
+    }
+    else if (welkePotato == "plastic") {
+        welkePotato = "toystory";
+    }
+
+    // potato transformeert
+    var aardappelKnop = document.querySelector("article > button");
+
+    if (welkePotato == "plastic") {
+        aardappelKnop.classList.add("plastic");
+    }
+    else if (welkePotato == "toystory") {
+        aardappelKnop.classList.remove("plastic");
+        aardappelKnop.classList.add("toystory");
+
+        var allButtons = document.querySelectorAll("section > button");
+
+    }
+
+   //kietelen
+
+
+    // props terug en weer klikbaar
+    allButtons.forEach( button => {
+        button.classList.remove("op");
+        button.removeAttribute("inert")
+    });
+
+    //  info weg
+    const gaPraten = document.querySelector("article:nth-of-type(2) section");
+    gaPraten.classList.remove('praten');
+
+    // info update
+    const deH2 = document.querySelector("article:nth-of-type(2) section h2");
+    const deP = document.querySelector("article:nth-of-type(2) section p");
+
+    if (welkePotato == "plastic") {
+        deH2.innerHTML = "plastic";
+        deP.innerHTML = "in delft moest ik kuststof zeggen";
+    }
+    else if (welkePotato == "toystory") {
+        deH2.innerHTML = "vette commercie";
+        deP.innerHTML = "weer retro in de verkoop";
+    }
+
+    
+}
